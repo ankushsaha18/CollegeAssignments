e18a10a7b0a2f3e02754fae887c9af5a159e793f
Ankush Saha <ankushsaha806@gmail.com>
2024-03-14 16:09:36 +0530

    Update Q4.java

--- SEM2/Assignment1/classass/Q4.java
+++ SEM2/Assignment1/classass/Q4.java
@@ -4,6 +4,20 @@ public class Q4 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char arr[] = { 'C', 'A', 'R' };
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j) {
+                    for (int k = 0; k < arr.length; k++) {
+                        if (j != k && i != k) {
+                            System.out.print(arr[i]);
+                            System.out.print(arr[j]);
+                            System.out.print(arr[k]);
+                            System.out.println();
+                        }
+                    }
+                }
+            }
+        }
 	}
 }
