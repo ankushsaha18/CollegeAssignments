74fba7e52cb5613a3a2037f73bf2243a0014d00f
Ankush Saha <ankushsaha806@gmail.com>
2024-03-14 18:52:54 +0530

    Update Q4.java

--- SEM2/Assignment1/classass/Q4.java
+++ SEM2/Assignment1/classass/Q4.java
@@ -3,19 +3,28 @@ package classass;
 public class Q4 {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		char arr[] = { 'C', 'A', 'R' };
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                for (int k = 0; k < arr.length; k++) {
-                    if (i != j && j != k && i != k) {
-                        System.out.print(arr[i]);
-                        System.out.print(arr[j]);
-                        System.out.print(arr[k]);
-                        System.out.println();
-                    }
-                }
+        char arr[] = { 'c', 'a', 'r', 'b', 'o', 'n' };
+        recPermute(0, arr);
+    }
+
+    static void recPermute(int index, char arr[]) {
+        if (index == arr.length) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i]);
             }
+            System.out.println();
+            return;
         }
-	}
+        for (int i = index; i < arr.length; i++) {
+            swap(i, index, arr);
+            recPermute(index + 1, arr);
+            swap(index, i, arr);
+        }
+    }
+
+    static void swap(int i, int j, char arr[]) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
