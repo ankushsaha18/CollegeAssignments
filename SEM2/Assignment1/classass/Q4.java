e0f0a79828c1b9bfe341c3d69059e01294fc22c4
Ankush Saha <ankushsaha806@gmail.com>
2024-03-14 16:12:39 +0530

    Update Q4.java

--- SEM2/Assignment1/classass/Q4.java
+++ SEM2/Assignment1/classass/Q4.java
@@ -7,14 +7,12 @@ public class Q4 {
 		char arr[] = { 'C', 'A', 'R' };
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (i != j) {
-                    for (int k = 0; k < arr.length; k++) {
-                        if (j != k && i != k) {
-                            System.out.print(arr[i]);
-                            System.out.print(arr[j]);
-                            System.out.print(arr[k]);
-                            System.out.println();
-                        }
+                for (int k = 0; k < arr.length; k++) {
+                    if (i != j && j != k && i != k) {
+                        System.out.print(arr[i]);
+                        System.out.print(arr[j]);
+                        System.out.print(arr[k]);
+                        System.out.println();
                     }
                 }
             }
