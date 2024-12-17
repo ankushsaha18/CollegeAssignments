ae41778d88033c42969a262beb240b0d3949f8f0
Ankush Saha <ankushsaha806@gmail.com>
2024-12-17 10:59:30 +0530

    changed

--- SEM3/CSW_1/Assignment6/Q7.java
+++ SEM3/CSW_1/Assignment6/Q7.java
@@ -8,12 +8,40 @@ public class Q7 {
         int y = sc.nextInt();
         System.out.println(power(x,y));
     }
-    public static  int power(int x,int y){
+    public static int power(int x,int y){
         int ans = 1;
         while (y > 0){
-            ans *= x;
-            y--;
+            if((y&1) == 1){
+                ans = multiplication(ans,x);
+            }
+            x = multiplication(x,x);
+            y >>= 1;
         }
         return ans;
+    }
+    public static int multiplication(int x,int y){
+        int ans = 0;
+        int sign = 1; // To get result of two negative numbers
+        if((x<0 && y>0) || (x>0 && y<0)){
+            sign = -1;
+        }
+        x = Math.abs(x);
+        y = Math.abs(y);
+        while (y > 0){
+            if((y&1) == 1){
+                ans = bitwiseAdd(ans,x);
+            }
+            x <<= 1;
+            y >>= 1;
+        }
+        return ans*sign;
+    }
+    public static int bitwiseAdd(int a,int b){
+        while (b != 0){
+            int carry = a&b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
     }
 }
