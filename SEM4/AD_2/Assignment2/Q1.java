ba61bf73e2c21ea61d70efe455420bbca1ae2233
Ankush Saha <ankushsaha806@gmail.com>
2025-02-26 19:07:24 +0530

    new uploaded

---
+++ SEM4/AD_2/Assignment2/Q1.java
@@ -0,0 +1,94 @@
+package Assignment2;
+
+import java.util.*;
+
+class DisjointSet {
+    private int[] parent;
+    private int[] size;
+
+    public DisjointSet(int n) {
+        parent = new int[n + 1];
+        size = new int[n + 1];
+        Arrays.fill(size, 1);
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int findSet(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findSet(parent[node]);
+    }
+
+    public void union(int u, int v) {
+        int u_ulp = findSet(u);
+        int v_ulp = findSet(v);
+        if (u_ulp == v_ulp) {
+            return;
+        }
+        if (size[u_ulp] < size[v_ulp]) {
+            parent[u_ulp] = v_ulp;
+            size[v_ulp] += size[u_ulp];
+        } else {
+            parent[v_ulp] = u_ulp;
+            size[u_ulp] += size[v_ulp];
+        }
+    }
+}
+
+public class Q1 {
+    public static int spanningTree(int V, List<List<List<Integer>>> adj) {
+        List<int[]> edges = new ArrayList<>();
+        int sum = 0;
+        DisjointSet d = new DisjointSet(V);
+
+        for (int u = 1; u <= V; u++) {
+            for (List<Integer> neighbor : adj.get(u)) {
+                int v = neighbor.get(0);
+                int wt = neighbor.get(1);
+                if (u < v) {
+                    edges.add(new int[]{wt, u, v});
+                }
+            }
+        }
+
+        edges.sort(Comparator.comparingInt(o -> o[0]));
+
+        for (int[] edge : edges) {
+            int wt = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+
+            if (d.findSet(u) != d.findSet(v)) {
+                sum += wt;
+                d.union(u, v);
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        int[][] edges = {
+                {1, 5, 2}, {1, 2, 5}, {1, 4, 2}, {5, 3, 7}, {2, 4, 2}, {2, 3, 1}
+        };
+
+        List<List<List<Integer>>> adj = new ArrayList<>();
+        for (int i = 0; i <= V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adj.get(u).add(Arrays.asList(v, wt));
+            adj.get(v).add(Arrays.asList(u, wt));
+        }
+
+        System.out.println("Minimum Weight = " + spanningTree(V, adj));
+    }
+}
---
+++ SEM4/AD_2/Assignment2/Q2.java
@@ -0,0 +1,55 @@
+package Assignment2;
+
+import java.util.*;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int V = 5;
+        int[][] edges = {
+                {1, 5, 2}, {1, 2, 5}, {1, 4, 2}, {5, 3, 7}, {2, 4, 2}, {2, 3, 1}
+        };
+
+        List<List<List<Integer>>> adj = new ArrayList<>();
+        for (int i = 0; i <= V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adj.get(u).add(Arrays.asList(v, wt));
+            adj.get(v).add(Arrays.asList(u, wt));
+        }
+
+        System.out.println("Minimum Weight = " + spanningTree(V, adj));
+    }
+    public static int spanningTree(int V, List<List<List<Integer>>> adj) {
+        int sum = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        boolean[] visited = new boolean[V+1];
+
+        pq.add(new int[]{0,1}); // first node format {weight,node}
+
+        while (!pq.isEmpty()){
+            int[] a = pq.poll();
+            int wt = a[0];
+            int node = a[1];
+
+            if(visited[node]) continue;
+
+            sum += wt;
+            visited[node] = true;
+            for(List<Integer> lst : adj.get(node)){
+                int adj_node = lst.get(0);  // adj list given in format {node,weight}
+                int adj_wt = lst.get(1);
+                if(!visited[adj_node]){
+                    pq.add(new int[]{adj_wt,adj_node});
+                }
+            }
+        }
+        return sum;
+    }
+}
