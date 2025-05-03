f0256ea88dbab443769249c1eba352389b52af74
Ankush Saha <ankushsaha806@gmail.com>
2025-05-03 13:33:55 +0530

    changed

--- SEM4/CSW_2/Assignment6/Q5/DeleteBST.java
+++ SEM4/CSW_2/Assignment6/Q5/DeleteBST.java
@@ -1,13 +1,7 @@
 package Assignment6.Q5;
 
 public class DeleteBST {
-    static class Node{
-        int info;
-        Node left,right;
 
-        public Node(int info) {
-            this.info = info;
-        }
     }
 
     public static Node delete(Node root,int val){
