3d77d9d4f14425fa081699a93d68409d910d6842
Ankush Saha <ankushsaha806@gmail.com>
2026-04-02 02:02:31 +0530

    pong completed

---
+++ SEM6/GPWC/Games/PongGame/Ball.h
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+
+class Ball{
+	private:
+		Vector2f m_Position;
+		CircleShape m_Shape;
+		
+		float m_Speed = 1500.0f;
+		float m_DirectionX = .2f;
+		float m_DirectionY = .2f;
+	public:
+		Ball(float startX,float startY);
+		
+		FloatRect getPosition();
+		CircleShape getShape();
+		float getXVelocity();
+		
+		void reboundSides();
+		void reboundBatOrTop();
+		void reboundBottom();
+		
+		void update(Time dt);
+};
--- SEM6/GPWC/Games/PongGame/Pong.cpp
+++ SEM6/GPWC/Games/PongGame/Pong.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "Bat.h"
+#include "Ball.h"
 #include <sstream>
 using namespace sf;
 
@@ -11,15 +13,39 @@ int main(){
 	int score = 0;
 	int life = 3;
 	Bat bat(1920/2,1080-20);
+	Ball ball(1920/2,0);
+	bool collision = false;
+	bool paused = true; // start paused
+	bool ballLost = false;
 	
 	Text hud;
+	Text messageText;
+	
+	// prepare sound
+	SoundBuffer overBuffer;
+	overBuffer.loadFromFile("faaa.mp3");
+	Sound over;
+	over.setBuffer(overBuffer);
 	
 	Font font;
-	font.loadFromFile("KOMIKAP_.ttf");
+	if (!font.loadFromFile("KOMIKAP_.ttf")) {
+		return -1;
+	}
+	
 	hud.setFont(font);
 	hud.setCharacterSize(75);
 	hud.setFillColor(Color::White);
 	hud.setPosition(20,20);
+	
+	// setup message text
+	messageText.setFont(font);
+	messageText.setCharacterSize(75);
+	messageText.setFillColor(Color::White);
+	messageText.setString("Press Enter to Start");
+	FloatRect textRect = messageText.getLocalBounds();
+	messageText.setOrigin(textRect.left + textRect.width/2.0f,
+						  textRect.top + textRect.height/2.0f);
+	messageText.setPosition(1920/2.0f,1080/2.0f);
 	
 	Clock clock;
 	
@@ -35,33 +61,99 @@ int main(){
 			window.close();
 		}
 		
-		// Handle Player Input
-		if(Keyboard::isKeyPressed(Keyboard::Left)){
-			bat.moveLeft();
-		}else{
-			bat.stopLeft();
+		// unpause / restart
+		if (paused && Keyboard::isKeyPressed(Keyboard::Enter)) {
+			paused = false;
+			score = 0;
+			life = 3;
+			ball = Ball(1920/2,0);
 		}
 		
-		if(Keyboard::isKeyPressed(Keyboard::Right)){
-			bat.moveRight();
-		}else{
-			bat.stopRight();
+		if(!paused){
+				// Handle Player Input
+			if(Keyboard::isKeyPressed(Keyboard::Left)){
+				bat.moveLeft();
+			}else{
+				bat.stopLeft();
+			}
+			
+			if(Keyboard::isKeyPressed(Keyboard::Right)){
+				bat.moveRight();
+			}else{
+				bat.stopRight();
+			}
+			
+			
+			// Update the bat, ball and HUD
+			
+			Time dt = clock.restart();
+			bat.update(dt);
+			ball.update(dt);
+			std::stringstream ss;
+			ss << "score:" << score << " Lives:" << life;
+			hud.setString(ss.str());
+			
+			
+			// Hit bottom
+			if(ball.getPosition().top > 1080 && !ballLost){
+				ballLost = true;
+				ball.reboundBottom();
+				life--;
+				if(life < 1){
+					score = 0;
+					life = 3;
+					paused = true;
+					ballLost = false;
+					messageText.setString("KHEL KHATAM BETA !!");
+					FloatRect textRect = messageText.getLocalBounds();
+					messageText.setOrigin(textRect.left + textRect.width/2.0f,
+										textRect.top + textRect.height/2.0f);
+					messageText.setPosition(1920/2.0f, 1080/2.0f);
+					hud.setPosition(20,20);
+					over.play();
+				}
+			}
+
+			// Reset ballLost when ball is back in play (top of screen area)
+			if(ball.getPosition().top < 1080){
+				ballLost = false;
+			}
+			
+			// Hit top
+			if(ball.getPosition().top < 0){
+				ball.reboundBatOrTop();
+				collision = false;
+			}
+			
+			// Hitting sides
+			if(ball.getPosition().left < 0 || 
+			   ball.getPosition().left + ball.getPosition().width > 1920){
+				ball.reboundSides();
+			}
+			
+			// reset collision when not touching bat
+			if (!ball.getPosition().intersects(bat.getPosition())) {
+				collision = false;
+			}
+			
+			// Hitting bat
+			if(ball.getPosition().intersects(bat.getPosition()) && collision == false){
+				ball.reboundBatOrTop();
+				score++;
+				collision = true;
+			}
 		}
-		
-		Time dt = clock.restart();
-		bat.update(dt);
-		std::stringstream ss;
-		ss << "score:" << score << " Lives:" << life;
-		hud.setString(ss.str());
-		
-		// Update the bat, ball and HUD
-		
-		
 		
 		// Draw all
 		window.clear();
-		window.draw(hud);
-		window.draw(bat.getShape());
+		if(paused){
+			window.draw(messageText);
+			window.draw(hud);
+		}else{
+			window.draw(hud);
+			window.draw(bat.getShape());
+			window.draw(ball.getShape());
+		}
 		window.display();
 	}
 	
