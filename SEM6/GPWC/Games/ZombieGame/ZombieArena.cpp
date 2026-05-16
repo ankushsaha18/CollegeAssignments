649888d1b3320074330d85ffff8d2cf0986bf0d4
Ankush Saha <ankushsaha806@gmail.com>
2026-05-17 00:31:00 +0530

    zombie

---
+++ SEM6/GPWC/Games/ZombieGame/ZombieArena.cpp
@@ -0,0 +1,585 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "Player.cpp"
+#include "CreateHorde.cpp"
+#include "Zombie.cpp"
+#include "Bullet.cpp"
+
+using namespace sf;
+
+int createBackground(VertexArray& rVA, IntRect arena);
+
+int main()
+{
+	// The game will always be in one of four states
+	enum class State { PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
+	// Start with the GAME_OVER state
+	State state = State::GAME_OVER;
+
+	
+	// Get the screen resolution and create an SFML window
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
+
+	RenderWindow window(VideoMode(resolution.x, resolution.y),
+		"Zombie Arena", Style::Fullscreen);
+
+	// Create a an SFML View for the main action
+	View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+	// Here is our clock for timing everything
+	Clock clock;
+	// How long has the PLAYING state been active
+	Time gameTimeTotal;
+
+	// Where is the mouse in relation to world coordinates
+	Vector2f mouseWorldPosition;
+	// Where is the mouse in relation to screen coordinates
+	Vector2i mouseScreenPosition;
+
+	// Create an instance of the Player class
+	Player player;
+
+	// The boundaries of the arena
+	IntRect arena;
+
+	// Create the background
+	VertexArray background;
+	// Load the texture for our background vertex array
+	Texture textureBackground; 
+	textureBackground.loadFromFile("graphics/background_sheet.png");	
+
+	// Menu background image
+	Texture textureMenuBackground;
+	textureMenuBackground.loadFromFile("graphics/background.png");
+	Sprite spriteMenuBackground;
+	spriteMenuBackground.setTexture(textureMenuBackground);
+	spriteMenuBackground.setScale(
+		resolution.x / textureMenuBackground.getSize().x,
+		resolution.y / textureMenuBackground.getSize().y);
+	RectangleShape menuOverlay(resolution);
+	menuOverlay.setFillColor(Color(0, 0, 0, 120));
+	
+	//Prepare for a horde of zombies
+	int numZombies;
+	int numZombiesAlive;
+	Zombie* zombies = NULL;
+	
+	//**********Bullets*****/
+	// 100 bullets should do
+	Bullet bullets[100];
+	int currentBullet = 0;
+	int bulletsSpare = 24; //24
+	int bulletsInClip = 6; //6
+	int clipSize = 6;
+	float fireRate = 10; //1
+	// When was the fire button last pressed?
+	Time lastPressed;
+
+	//********Use Crosshair in place of mouse pointer******/
+	// Hide the mouse pointer and replace it with crosshair
+	window.setMouseCursorVisible(true);
+	Sprite spriteCrosshair;
+	Texture textureCrosshair;
+	textureCrosshair.loadFromFile("graphics/crosshair.png");
+	spriteCrosshair.setTexture(textureCrosshair);
+	spriteCrosshair.setOrigin(25, 25);
+
+	//*******About the game
+	int score = 0;
+	int hiScore = 0;
+
+	// Text shown when the game is not actively playing
+	Font font;
+	font.loadFromFile("font/zombiecontrol.ttf");
+
+	Text titleText;
+	titleText.setFont(font);
+	titleText.setCharacterSize(80);
+	titleText.setFillColor(Color::White);
+	titleText.setString("Zombie Arena");
+	titleText.setPosition(80, 80);
+
+	Text messageText;
+	messageText.setFont(font);
+	messageText.setCharacterSize(38);
+	messageText.setFillColor(Color::White);
+	messageText.setPosition(80, 210);
+
+	// Sound effects
+	SoundBuffer shootBuffer;
+	shootBuffer.loadFromFile("sound/shoot.wav");
+	Sound shootSound;
+	shootSound.setBuffer(shootBuffer);
+
+	SoundBuffer reloadBuffer;
+	reloadBuffer.loadFromFile("sound/reload.wav");
+	Sound reloadSound;
+	reloadSound.setBuffer(reloadBuffer);
+
+	SoundBuffer reloadFailedBuffer;
+	reloadFailedBuffer.loadFromFile("sound/reload_failed.wav");
+	Sound reloadFailedSound;
+	reloadFailedSound.setBuffer(reloadFailedBuffer);
+
+	SoundBuffer hitBuffer;
+	hitBuffer.loadFromFile("sound/hit.wav");
+	Sound hitSound;
+	hitSound.setBuffer(hitBuffer);
+
+	SoundBuffer splatBuffer;
+	splatBuffer.loadFromFile("sound/splat.wav");
+	Sound splatSound;
+	splatSound.setBuffer(splatBuffer);
+
+	SoundBuffer powerupBuffer;
+	powerupBuffer.loadFromFile("sound/powerup.wav");
+	Sound powerupSound;
+	powerupSound.setBuffer(powerupBuffer);
+
+
+	// The main game loop
+	while (window.isOpen())
+	{
+		/*
+		************
+		Handle input
+		************
+		*/
+
+		// Handle events
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::KeyPressed)
+			{
+				// Pause a game while playing
+				if (event.key.code == Keyboard::Return &&
+					state == State::PLAYING)
+				{
+					state = State::PAUSED;
+				}
+
+				// Restart while paused
+				else if (event.key.code == Keyboard::Return &&
+					state == State::PAUSED)
+				{
+					state = State::PLAYING;
+					// Reset the clock so there isn't a frame jump
+					clock.restart();
+				}
+
+				// Start a new game while in GAME_OVER state
+				else if (event.key.code == Keyboard::Return &&
+					state == State::GAME_OVER)
+				{
+					state = State::LEVELING_UP;
+				}
+
+				if (state == State::PLAYING)
+				{
+					// Reloading
+					if (event.key.code == Keyboard::R)
+					{
+						if (bulletsSpare >= clipSize)
+						{
+							// Plenty of bullets. Reload.
+							bulletsInClip = clipSize;
+							bulletsSpare -= clipSize;
+							reloadSound.play();
+						}
+						else if (bulletsSpare > 0)
+						{
+							// Only few bullets left
+							bulletsInClip = bulletsSpare;
+							bulletsSpare = 0;
+							reloadSound.play();
+						}
+						else
+						{
+							reloadFailedSound.play();
+						}
+					}
+				}
+
+			}
+		}// End event polling
+
+
+		 // Handle the player quitting
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		// Handle controls while playing
+		if (state == State::PLAYING)
+		{
+			// Handle the pressing and releasing of the WASD keys
+			if (Keyboard::isKeyPressed(Keyboard::W))
+			{
+				player.moveUp();
+			}
+			else
+			{
+				player.stopUp();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::S))
+			{
+				player.moveDown();
+			}
+			else
+			{
+				player.stopDown();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::A))
+			{
+				player.moveLeft();
+			}
+			else
+			{
+				player.stopLeft();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::D))
+			{
+				player.moveRight();
+			}
+			else
+			{
+				player.stopRight();
+			}
+
+			//**********Fire a bullet******/
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+
+				if (gameTimeTotal.asMilliseconds()
+					- lastPressed.asMilliseconds()
+					> 1000 / fireRate && bulletsInClip > 0)
+				{
+
+					// Pass the centre of the player 
+					// and the centre of the cross-hair
+					// to the shoot function
+					bullets[currentBullet].shoot(
+						player.getCenter().x, player.getCenter().y,
+						mouseWorldPosition.x, mouseWorldPosition.y);
+
+					currentBullet++;
+					if (currentBullet > 99)
+					{
+						currentBullet = 0;
+					}
+					lastPressed = gameTimeTotal;
+
+					bulletsInClip--;
+					shootSound.play();
+				}
+
+			}// End fire a bullet
+
+		}// End WASD while playing
+
+		 // Handle the levelling up state
+		if (state == State::LEVELING_UP)
+		{
+			// Handle the player levelling up
+				if (Keyboard::isKeyPressed(Keyboard::Num1) ||
+					Keyboard::isKeyPressed(Keyboard::Num2) ||
+					Keyboard::isKeyPressed(Keyboard::Num3) ||
+				Keyboard::isKeyPressed(Keyboard::Num4) ||
+				Keyboard::isKeyPressed(Keyboard::Num5) ||
+				Keyboard::isKeyPressed(Keyboard::Num6))
+				{
+					state = State::PLAYING;
+					powerupSound.play();
+				}
+
+			if (state == State::PLAYING)
+			{
+				// Prepare thelevel
+				// We will modify the next two lines later
+				arena.width = 500;
+				arena.height = 500;
+				arena.left = 0;
+				arena.top = 0;
+
+				// Pass the vertex array by reference 
+				// to the createBackground function
+				int tileSize = createBackground(background, arena);
+
+				// Spawn the player in the middle of the arena
+				player.spawn(arena, resolution, tileSize);
+
+				//Create a horde of zombies
+				   numZombies = 4;
+
+				// Delete the previously allocated memory (if it exists)
+				   delete[] zombies;
+				   zombies = createHorde(numZombies, arena);
+				   numZombiesAlive = numZombies;
+
+				// Reset the clock so there isn't a frame jump
+				clock.restart();
+			}
+		}// End levelling up
+
+		 /*
+		 ****************
+		 UPDATE THE FRAME
+		 ****************
+		 */
+		if (state == State::PLAYING)
+		{
+			// Update the delta time
+			Time dt = clock.restart();
+			// Update the total game time
+			gameTimeTotal += dt;
+			// Make a decimal fraction of 1 from the delta time
+			float dtAsSeconds = dt.asSeconds();
+
+			// Where is the mouse pointer
+			mouseScreenPosition = Mouse::getPosition();
+
+			// Convert mouse position to world coordinates of mainView
+			mouseWorldPosition = window.mapPixelToCoords(
+				Mouse::getPosition(), mainView);
+
+			//*******Set the crosshair to the //
+			//*******mouse world location****//
+			spriteCrosshair.setPosition(mouseWorldPosition);
+
+			// Update the player
+			player.update(dtAsSeconds, Mouse::getPosition());
+
+			// Make a note of the players new position
+			Vector2f playerPosition(player.getCenter());
+
+			// Make the view centre around the player				
+			mainView.setCenter(player.getCenter());
+
+			//Loop through each Zombie and update them
+			for (int i = 0; i < numZombies; i++)
+			{
+				if (zombies[i].isAlive())
+				{
+					zombies[i].update(dt.asSeconds(), playerPosition);
+				}
+			}
+			
+			//********Update any bullets that are in-flight****/
+			for (int i = 0; i < 100; i++)
+			{
+				if (bullets[i].isInFlight())
+				{
+					bullets[i].update(dtAsSeconds);
+				}
+			}
+
+			//*******Collision detection*****/
+			//*****Have any zombies been shot?****/
+			for (int i = 0; i < 100; i++)
+			{
+				for (int j = 0; j < numZombies; j++)
+				{
+					if (bullets[i].isInFlight() &&
+						zombies[j].isAlive())
+					{
+						if (bullets[i].getPosition().intersects
+							(zombies[j].getPosition()))
+						{
+							//*****Stop the bullet
+							bullets[i].stop();
+
+							//*******Register the hit and see if it was a kill
+							if (zombies[j].hit()) {
+								splatSound.play();
+								//*******Not just a hit but a kill too
+								score += 10;
+								if (score >= hiScore)
+								{
+									hiScore = score;
+								}
+
+								numZombiesAlive--;
+
+								//******When all the zombies are dead (again)
+								if (numZombiesAlive == 0) {
+									state = State::LEVELING_UP;
+								}
+							}
+							else
+							{
+								hitSound.play();
+							}
+
+						}
+					}
+
+				}
+			}//******End zombie being shot
+
+			 //******Have any zombies touched the player			
+			for (int i = 0; i < numZombies; i++)
+			{
+				if (player.getPosition().intersects
+					(zombies[i].getPosition()) && zombies[i].isAlive())
+				{
+
+					if (player.hit(gameTimeTotal))
+					{
+						hitSound.play();
+					}
+
+					if (player.getHealth() <= 0)
+					{
+						state = State::GAME_OVER;
+
+					}
+				}
+			}//******End player touched
+
+		}//*******End updating the scene
+
+		 /*
+		 **************
+		 Draw the scene
+		 **************
+		 */
+
+		window.clear();
+
+		if (state == State::PLAYING)
+		{
+			// set the mainView to be displayed in the window
+			// And draw everything related to it
+			window.setView(mainView);
+
+			// Draw the background
+			window.draw(background, &textureBackground);
+
+			//Draw the zombies
+			for (int i = 0; i < numZombies; i++)
+			{
+				window.draw(zombies[i].getSprite());
+			}
+			
+			//****** Draw bullets in flight *****/
+			for (int i = 0; i < 100; i++)
+			{
+				if (bullets[i].isInFlight())
+				{
+					window.draw(bullets[i].getShape());
+				}
+			}
+
+			// Draw the player
+			window.draw(player.getSprite());
+
+			//******* Draw the crosshair
+			window.draw(spriteCrosshair);
+		}
+
+		if (state == State::LEVELING_UP)
+		{
+			window.setView(window.getDefaultView());
+			messageText.setString("Choose an upgrade:\n\n1 - Start next wave\n2 - Start next wave\n3 - Start next wave\n4 - Start next wave\n5 - Start next wave\n6 - Start next wave");
+			window.draw(spriteMenuBackground);
+			window.draw(menuOverlay);
+			window.draw(titleText);
+			window.draw(messageText);
+		}
+
+		if (state == State::PAUSED)
+		{
+			window.setView(window.getDefaultView());
+			messageText.setString("Paused\n\nPress Enter to continue\nPress Escape to quit");
+			window.draw(spriteMenuBackground);
+			window.draw(menuOverlay);
+			window.draw(titleText);
+			window.draw(messageText);
+		}
+
+		if (state == State::GAME_OVER)
+		{
+			window.setView(window.getDefaultView());
+			messageText.setString("Press Enter to start\n\nMove: W A S D\nShoot: Left mouse button\nReload: R\nQuit: Escape");
+			window.draw(spriteMenuBackground);
+			window.draw(menuOverlay);
+			window.draw(titleText);
+			window.draw(messageText);
+		}
+
+		window.display();
+
+	}// End game loop
+
+	return 0;
+}
+
+int createBackground(VertexArray& rVA, IntRect arena)
+{
+	// Anything we do to rVA we are actually doing to background (in the main function)
+
+	// How big is each tile/texture
+	const int TILE_SIZE = 50;
+	const int TILE_TYPES = 3;
+	const int VERTS_IN_QUAD = 4;
+
+	int worldWidth = arena.width / TILE_SIZE;
+	int worldHeight = arena.height / TILE_SIZE;
+
+	// What type of primitive are we using?
+	rVA.setPrimitiveType(Quads);
+
+	// Set the size of the vertex array
+	rVA.resize(worldWidth * worldHeight * VERTS_IN_QUAD);
+
+	// Start at the beginning of the vertex array
+	int currentVertex = 0;
+
+	for (int w = 0; w < worldWidth; w++)
+	{
+		for (int h = 0; h < worldHeight; h++)
+		{
+			// Position each vertex in the current quad
+			rVA[currentVertex + 0].position = Vector2f(w * TILE_SIZE, h * TILE_SIZE);
+			rVA[currentVertex + 1].position = Vector2f((w * TILE_SIZE) + TILE_SIZE, h * TILE_SIZE);
+			rVA[currentVertex + 2].position = Vector2f((w * TILE_SIZE) + TILE_SIZE, (h * TILE_SIZE) + TILE_SIZE);
+			rVA[currentVertex + 3].position = Vector2f((w * TILE_SIZE), (h * TILE_SIZE) + TILE_SIZE);
+
+			// Define the position in the Texture to draw for current quad
+			// Either mud, stone, grass or wall
+			if (h == 0 || h == worldHeight - 1 || w == 0 || w == worldWidth - 1)
+			{
+				// Use the wall texture
+				rVA[currentVertex + 0].texCoords = Vector2f(0, 0 + TILE_TYPES * TILE_SIZE);
+				rVA[currentVertex + 1].texCoords = Vector2f(TILE_SIZE, 0 + TILE_TYPES * TILE_SIZE);
+				rVA[currentVertex + 2].texCoords = Vector2f(TILE_SIZE, TILE_SIZE + TILE_TYPES * TILE_SIZE);
+				rVA[currentVertex + 3].texCoords = Vector2f(0, TILE_SIZE + TILE_TYPES * TILE_SIZE);
+			}
+			else
+			{
+				// Use a random floor texture
+				srand((int)time(0) + h * w - h);
+				int mOrG = (rand() % TILE_TYPES);
+				int verticalOffset = mOrG * TILE_SIZE;
+
+				rVA[currentVertex + 0].texCoords = Vector2f(0, 0 + verticalOffset);
+				rVA[currentVertex + 1].texCoords = Vector2f(TILE_SIZE, 0 + verticalOffset);
+				rVA[currentVertex + 2].texCoords = Vector2f(TILE_SIZE, TILE_SIZE + verticalOffset);
+				rVA[currentVertex + 3].texCoords = Vector2f(0, TILE_SIZE + verticalOffset);
+
+			}
+
+			// Position ready for the next for vertices
+			currentVertex = currentVertex + VERTS_IN_QUAD;
+		}
+	}
+
+	return TILE_SIZE;
+}
