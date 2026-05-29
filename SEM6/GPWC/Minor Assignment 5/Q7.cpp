8d9285979254c5732fcaeed08bf6cec3cf4f948d
Ankush Saha <ankushsaha806@gmail.com>
2026-05-29 23:37:58 +0530

    new

--- SEM6/GPWC/Minor Assignment 5/Q7.cpp
+++ SEM6/GPWC/Minor Assignment 5/Q7.cpp
@@ -1,33 +1,56 @@
 # include <SFML/Graphics.hpp>
 using namespace sf;
 
-void displayBackground(sf::VertexArray& rVA, sf::IntRect arena)
+void displayBackground(VertexArray& rVA, IntRect arena)
 {
     const int TILE_SIZE = 50;
 
     int width = arena.width / TILE_SIZE;
     int height = arena.height / TILE_SIZE;
 
-    rVA.setPrimitiveType(sf::Quads);
+    rVA.setPrimitiveType(Quads);
     rVA.resize(width * height * 4);
 
     int currentVertex = 0;
 
-    for(int w=0; w<width; w++)
+    for(int w = 0; w < width; w++)
     {
-        for(int h=0; h<height; h++)
+        for(int h = 0; h < height; h++)
         {
-            sf::Vertex* quad = &rVA[currentVertex];
+            Vertex* quad = &rVA[currentVertex];
 
-            quad[0].position = sf::Vector2f(w*TILE_SIZE, h*TILE_SIZE);
-            quad[1].position = sf::Vector2f((w+1)*TILE_SIZE, h*TILE_SIZE);
-            quad[2].position = sf::Vector2f((w+1)*TILE_SIZE,(h+1)*TILE_SIZE);
-            quad[3].position = sf::Vector2f(w*TILE_SIZE,(h+1)*TILE_SIZE);
+            // Position of tile
+            quad[0].position = Vector2f(w*TILE_SIZE, h*TILE_SIZE);
+            quad[1].position = Vector2f((w+1)*TILE_SIZE, h*TILE_SIZE);
+            quad[2].position = Vector2f((w+1)*TILE_SIZE, (h+1)*TILE_SIZE);
+            quad[3].position = Vector2f(w*TILE_SIZE, (h+1)*TILE_SIZE);
 
-            quad[0].texCoords = sf::Vector2f(0,50);
-            quad[1].texCoords = sf::Vector2f(50,50);
-            quad[2].texCoords = sf::Vector2f(50,100);
-            quad[3].texCoords = sf::Vector2f(0,100);
+            // Border = Wall
+            if(w == 0 || h == 0 || w == width-1 || h == height-1)
+            {
+                quad[0].texCoords = Vector2f(0,150);
+                quad[1].texCoords = Vector2f(50,150);
+                quad[2].texCoords = Vector2f(50,200);
+                quad[3].texCoords = Vector2f(0,200);
+            }
+
+            // Diagonal = Grass
+            else if(w == h)
+            {
+                quad[0].texCoords = Vector2f(0,50);
+                quad[1].texCoords = Vector2f(50,50);
+                quad[2].texCoords = Vector2f(50,100);
+                quad[3].texCoords = Vector2f(0,100);
+            }
+
+            // Remaining area = Mud-1
+            else
+            {
+                quad[0].texCoords = Vector2f(0,0);
+                quad[1].texCoords = Vector2f(50,0);
+                quad[2].texCoords = Vector2f(50,50);
+                quad[3].texCoords = Vector2f(0,50);
+            }
 
             currentVertex += 4;
         }
